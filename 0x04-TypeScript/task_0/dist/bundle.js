4053bcf0d09e64f9566483ea542fb4c7af8e9dee
SylvesterK-code <sylvesterkeystarling@gmail.com>
2025-07-13 18:36:26 +0000

    Add javaScript app-interface for student

---
+++ 0x04-TypeScript/task_0/dist/bundle.js
@@ -0,0 +1,100 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./js/main.ts");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./js/main.ts":
+/*!********************!*\
+  !*** ./js/main.ts ***!
+  \********************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("var student1 = {\r\n    firstName: \"Sylvester\",\r\n    lastName: \"Nyadzinnor\",\r\n    age: 28,\r\n    location: \"Accra\"\r\n};\r\nvar student2 = {\r\n    firstName: \"Ama\",\r\n    lastName: \"Mensah\",\r\n    age: 25,\r\n    location: \"Kumasi\"\r\n};\r\nvar studentsList = [student1, student2];\r\nvar table = document.createElement(\"table\");\r\nvar headerRow = document.createElement(\"tr\");\r\nvar firstNameHeader = document.createElement(\"th\");\r\nfirstNameHeader.textContent = \"First Name\";\r\nvar locationHeader = document.createElement(\"th\");\r\nlocationHeader.textContent = \"Location\";\r\nheaderRow.appendChild(firstNameHeader);\r\nheaderRow.appendChild(locationHeader);\r\ntable.appendChild(headerRow);\r\nstudentsList.forEach(function (student) {\r\n    var row = document.createElement(\"tr\");\r\n    var firstNameCell = document.createElement(\"td\");\r\n    firstNameCell.textContent = student.firstName;\r\n    var locationCell = document.createElement(\"td\");\r\n    locationCell.textContent = student.location;\r\n    row.appendChild(firstNameCell);\r\n    row.appendChild(locationCell);\r\n    table.appendChild(row);\r\n});\r\ndocument.body.appendChild(table);\r\n\n\n//# sourceURL=webpack:///./js/main.ts?");
+
+/***/ })
+
+/******/ });
